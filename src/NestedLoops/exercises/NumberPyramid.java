51bfbf6b7f46d409ec21d7627f8feb3591c7d1b3
oziriuz <99746294+oziriuz@users.noreply.github.com>
2022-03-17 20:13:02 +0200

    nested

---
+++ src/NestedLoops/exercises/NumberPyramid.java
@@ -0,0 +1,11 @@
+package NestedLoops.exercises;
+
+import java.util.Scanner;
+
+public class NumberPyramid {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+}
--- src/NestedLoops/lab/Travelling.java
+++ src/NestedLoops/lab/Travelling.java
@@ -6,6 +6,18 @@ public class Travelling {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String command = scanner.nextLine();
 
+        while (!command.equals("End")) {
+            double target = Double.parseDouble(scanner.nextLine());
+            double money = 0;
+            double sumMoney = 0;
+            while (sumMoney < target) {
+                money = Double.parseDouble(scanner.nextLine());
+                sumMoney += money;
+            }
+            System.out.printf("Going to %s!%n", command);
+            command = scanner.nextLine();
+        }
     }
 }
